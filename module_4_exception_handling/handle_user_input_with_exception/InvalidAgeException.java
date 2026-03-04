6a9d74d1b50773c46be614517014fa5224007287
AjayVincent <161117924+AjayVincent@users.noreply.github.com>
2026-03-04 19:15:34 +0530

    Delete module_4_exception_handling directory

--- module_4_exception_handling/handle_user_input_with_exception/InvalidAgeException.java
+++
@@ -1,7 +0,0 @@
-package org.example.module_4_exception_handling.handle_user_input_with_exception;
-
-public class InvalidAgeException extends Exception{
-    InvalidAgeException(String msg) {
-        super(msg);
-    }
-}
